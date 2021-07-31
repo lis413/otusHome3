f40da825f04f38bf58bcdc434de5f8375e91e1b1
lis154 <lis154@mail.ru>
2021-07-31 11:54:12 +0300

    5

--- L06-reflection/src/main/java/ru/otus/homework/tests/TestFramework.java
+++ L06-reflection/src/main/java/ru/otus/homework/tests/TestFramework.java
@@ -3,52 +3,55 @@ package ru.otus.homework.tests;
 import ru.otus.homework.anotation.After;
 import ru.otus.homework.anotation.Before;
 import ru.otus.homework.anotation.Test;
-import ru.otus.homework.tests.TestClass;
 
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TestFramework {
-    static List<Method> listBefore = new ArrayList<>();
-    static List<Method> listAfter = new ArrayList<>();
-    static List<Method> listTest = new ArrayList<>();
     public static void analyze(Class<?> clazz) {
-        divideMethode(clazz);
-            int stat = 0;
-            int error = 0;
-            for (Method method : listTest) {
-                Object object = null;
-                try {
-                    object = clazz.newInstance();
-                for (Method before : listBefore) {
-                    before.invoke(object);
-                }
+        List<Method> listBefore = new ArrayList<>();
+        List<Method> listAfter = new ArrayList<>();
+        List<Method> listTest = new ArrayList<>();
+        divideMethode(clazz, listBefore, listAfter, listTest);
+        int stat = 0;
+        int error = 0;
+        for (Method method : listTest) {
+            Object object = null;
+            try {
+                object = clazz.getDeclaredConstructor().newInstance();
+                printMethod(listBefore, object);
                 method.invoke(object);
                 stat++;
-                } catch (Exception e) {
-                    error++;
-                    System.out.println("Exception = " + e);
-                } finally {
-                    for (Method after : listAfter) {
-                        try {
-                            after.invoke(object);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
+            } catch (Exception e) {
+                error++;
+                System.out.println("Exception = " + e);
+            } finally {
+                printMethod(listAfter, object);
             }
-        System.out.println("Всего выполнено = " + stat);
-        System.out.println("Выполнено успешно = " + (stat - error));
-        System.out.println("Выполнено с ошибкой = " + error);
+        }
+        printStat(stat, error);
 
     }
 
-    private static void divideMethode(Class<?> clazz){
+    private static void printStat(int stat, int error) {
+        System.out.println("Всего выполнено = " + stat);
+        System.out.println("Выполнено успешно = " + (stat - error));
+        System.out.println("Выполнено с ошибкой = " + error);
+    }
+
+    private static void printMethod(List<Method> methods, Object object) {
+        for (Method before : methods) {
+            try {
+                before.invoke(object);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void divideMethode(Class<?> clazz, List<Method> listBefore, List<Method> listAfter, List<Method> listTest) {
         Method[] methods = clazz.getMethods();
         for (Method method : methods) {
             if (method.isAnnotationPresent(Before.class)) {
