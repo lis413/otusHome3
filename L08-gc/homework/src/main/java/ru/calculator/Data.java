85a5e2b76066cea346dde5e901b605f1d5a8cb2b
lis154 <lis154@mail.ru>
2021-08-07 13:17:10 +0300

    5

---
+++ L08-gc/homework/src/main/java/ru/calculator/Data.java
@@ -0,0 +1,16 @@
+package ru.calculator;
+
+public class Data {
+    private Integer value;
+
+    public Data(){
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+}
