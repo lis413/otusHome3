4ee46216ea892bb607673d19085004f23386d12d
lis154 <lis154@mail.ru>
2021-11-12 14:27:10 +0300

    commmit L28

---
+++ L28-springDataJdbc/src/main/java/ru/otus/crm/repository/ClientRepository.java
@@ -0,0 +1,17 @@
+package ru.otus.crm.repository;
+
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import ru.otus.crm.model.Client;
+
+import java.util.List;
+import java.util.Set;
+
+public interface ClientRepository extends CrudRepository<Client, Long> {
+
+    public List<Client> findAll();
+
+    @Query("select max(id) from client")
+    public Long findMaxId();
+
+}
